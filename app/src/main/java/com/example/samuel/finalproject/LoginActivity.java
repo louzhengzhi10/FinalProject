814e059965b018f1f4ac7b5937cb5921345a34a6
Samuel Lou <zlou4@illinois.edu>
2016-11-11 16:47:56 -0600

    Register

--- app/src/main/java/com/example/samuel/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/samuel/finalproject/LoginActivity.java
@@ -5,7 +5,6 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -218,7 +217,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private void jump_to_register() {
-        Intent fp=new Intent(getApplicationContext(), Register.class);
+        Intent fp=new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(fp);
     }
 
---
+++ app/src/main/java/com/example/samuel/finalproject/RegisterActivity.java
@@ -0,0 +1,68 @@
+package com.example.samuel.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Samuel on 11/5/16.
+ */
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+public class RegisterActivity extends Activity {
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private EditText mNameView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        mNameView = (EditText) findViewById(R.id.name);
+        mNameView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.register || id == EditorInfo.IME_NULL) {
+                    attemptRegister();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailRegisterButton = (Button) findViewById(R.id.email_register_button);
+        mEmailRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptRegister();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private void attemptRegister() {
+
+    }
+
+
+
+}
